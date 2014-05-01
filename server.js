03451cc46bb40cc513ec98bc3a9d6ba278b8258d
L33t Hax0r <zl3kh@virginia.edu>
2014-05-01 18:00:51 -0400

    debug

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 var express = require("express")
-, path = require('path')
-, mongoose = require('mongoose')
-, models = require('./models');
+, path = require('path');
 
 var app = express();
 
-var uristring = process.env.MONGOLAB_URI;
-// 'mongodb://localhost/myMongodbDatabase';
-
+require('./routes')(app);
 
 // Points Express to a folder where you keep static files
 // e.g. css or client side js files
 app.use(express.static(path.normalize(__dirname) + '/public'))
 
+// Tell express to use its built in error handler
+app.use(express.errorHandler());
+
+app.configure(function() {
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+})
 
 // These 3 lines tell express that we are going to be rendering html files
 // held in the public directory which should be in the same directory as this file
@@ -20,24 +23,9 @@ app.set('views', path.normalize(__dirname) + '/public/html');
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
-// Tell express to use its built in error handler
-app.use(express.logger('dev'));
-app.use(express.errorHandler());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-
-require('./routes')(app);
-
-mongoose.connect(uristring, function(err, res) {
-	if (err) {
-		console.log("Error" + err);
-	} else {
-		console.log("Successfully connected to mongodb at the url: " + uristring);
-		var port = Number(process.env.PORT || 8000);
-		app.listen(port);
-		console.log("App listening on port: " + port);
-	}
-});
 
 
+
+var port = Number(process.env.PORT || 8000);
+app.listen(port);
+console.log("App listening on port: " + port);
