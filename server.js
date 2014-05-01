763768cd30ff0dfa2ec3cabd45ab17c028791424
L33t Hax0r <zl3kh@virginia.edu>
2014-05-01 17:52:36 -0400

    heroku

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 var express = require("express")
 , path = require('path')
-, mongoose = require ('mongoose');
+, mongoose = require('mongoose')
+, models = require('./models');
 
 var app = express();
 
+var uristring = process.env.MONGOLAB_URI;
+// 'mongodb://localhost/myMongodbDatabase';
 
 
 // Points Express to a folder where you keep static files
 // e.g. css or client side js files
-app.use(express.static(path.normalize(__dirname) + '/public'));
+app.use(express.static(path.normalize(__dirname) + '/public'))
 
-var uristring = process.env.MONGOLAB_URL;
-// Tell express to use its built in error handler
-app.use(express.errorHandler());
-
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser()); // parsing post request
-app.use(exprees.methodOverride());
-app.use(express.router); // allow to work with your router
 
 // These 3 lines tell express that we are going to be rendering html files
 // held in the public directory which should be in the same directory as this file
@@ -25,18 +20,24 @@ app.set('views', path.normalize(__dirname) + '/public/html');
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
+// Tell express to use its built in error handler
+app.use(express.logger('dev'));
+app.use(express.errorHandler());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
 require('./routes')(app);
 
-mongoose.connect(uristring, function(err, res){
-    if(err)
-    {
-	console.log("err" + err);
-}
-    else
-    {
-	var port = Number(process.env.PORT) || 8000;
-			  app.listen(port);
-			  console.log("App listening on port: " + port);
-    }
-};
+mongoose.connect(uristring, function(err, res) {
+	if (err) {
+		console.log("Error" + err);
+	} else {
+		console.log("Successfully connected to mongodb at the url: " + uristring);
+		var port = Number(process.env.PORT || 8000);
+		app.listen(port);
+		console.log("App listening on port: " + port);
+	}
+});
+
 
